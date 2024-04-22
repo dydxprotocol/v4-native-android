8b64fbd5ec43401e81d26eb936944961a6980361
Rui <rui@dydx.exchange>
2024-04-22 13:12:58 -0700

    Add retry to trade and close order submission.

--- v4/feature/trade/src/main/java/exchange/dydx/trading/feature/trade/streams/TradeStream.kt
+++ v4/feature/trade/src/main/java/exchange/dydx/trading/feature/trade/streams/TradeStream.kt
@@ -4,15 +4,11 @@ import dagger.hilt.android.scopes.ActivityRetainedScoped
 import exchange.dydx.abacus.output.SubaccountOrder
 import exchange.dydx.abacus.state.model.TradeInputField
 import exchange.dydx.dydxstatemanager.AbacusStateManagerProtocol
-import exchange.dydx.trading.common.di.CoroutineScopes
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 interface TradeStreaming {
@@ -28,12 +24,12 @@ interface MutableTradeStreaming : TradeStreaming {
 @ActivityRetainedScoped
 class TradeStream @Inject constructor(
     val abacusStateManager: AbacusStateManagerProtocol,
-    @CoroutineScopes.App val appScope: CoroutineScope
 ) : MutableTradeStreaming {
 
     private var _submissionStatus: MutableStateFlow<AbacusStateManagerProtocol.SubmissionStatus?> =
         MutableStateFlow(null)
-    override val submissionStatus: Flow<AbacusStateManagerProtocol.SubmissionStatus?> = _submissionStatus
+    override val submissionStatus: Flow<AbacusStateManagerProtocol.SubmissionStatus?> =
+        _submissionStatus
 
     override val lastOrder: Flow<SubaccountOrder?> =
         combine(
@@ -57,9 +53,8 @@ class TradeStream @Inject constructor(
             .distinctUntilChanged()
 
     override fun submitTrade() {
-        _submissionStatus.update { null }
-        appScope.launch {
-            val tradeInput = abacusStateManager.state.tradeInput.first() ?: return@launch
+        if (abacusStateManager.state.tradeInput != null) {
+            _submissionStatus.update { null }
 
             abacusStateManager.placeOrder { submissionStatus ->
                 if (submissionStatus == AbacusStateManagerProtocol.SubmissionStatus.Success) {
@@ -71,9 +66,8 @@ class TradeStream @Inject constructor(
     }
 
     override fun closePosition() {
-        _submissionStatus.update { null }
-        appScope.launch {
-            val closePositionInput = abacusStateManager.state.closePositionInput.first() ?: return@launch
+        if (abacusStateManager.state.closePositionInput != null) {
+            _submissionStatus.update { null }
 
             abacusStateManager.closePosition { submissionStatus ->
                 _submissionStatus.update { submissionStatus }
