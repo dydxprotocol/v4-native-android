b2412d6bca595c6e72d54c205c59589bc61b0deb
Rui <rui@dydx.exchange>
2024-05-16 10:27:43 -0700

    MOB-511 Fix JS runner in webview of older devices

--- v4/integration/javascript/src/main/java/exchange/dydx/integration/javascript/JsEngine.kt
+++ v4/integration/javascript/src/main/java/exchange/dydx/integration/javascript/JsEngine.kt
@@ -1,6 +1,6 @@
 package exchange.dydx.integration.javascript
 
 interface JsEngine {
-    suspend fun runJs(function: String, params: List<String>, callback: ResultCallback)
-    suspend fun runJs(script: String, callback: ResultCallback)
+    fun runJs(function: String, params: List<String>, callback: ResultCallback)
+    fun runJs(script: String, callback: ResultCallback)
 }
