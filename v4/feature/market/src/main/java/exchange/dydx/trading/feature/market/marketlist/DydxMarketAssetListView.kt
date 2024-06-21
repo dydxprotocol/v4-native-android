55b4be52bc3728209167f4381fee4bcca516e570
Rui <rui@dydx.exchange>
2024-06-21 11:06:09 -0700

    LInt

--- v4/feature/market/src/main/java/exchange/dydx/trading/feature/market/marketlist/DydxMarketAssetListView.kt
+++ v4/feature/market/src/main/java/exchange/dydx/trading/feature/market/marketlist/DydxMarketAssetListView.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
