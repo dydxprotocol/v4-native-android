da848d05a48465462f8da78ebb596b3c1dcfddf8
Prashan Dharmasena <prashan@dydx.exchange>
2024-09-12 16:14:22 -0400

    Bump abacus to 1.11.2

--- v4/integration/dydxStateManager/src/main/java/exchange/dydx/dydxstatemanager/AbacusStateManager.kt
+++ v4/integration/dydxStateManager/src/main/java/exchange/dydx/dydxstatemanager/AbacusStateManager.kt
@@ -199,8 +199,6 @@ class AbacusStateManager @Inject constructor(
 
         appConfigsV2.staticTyping = featureFlags.isFeatureEnabled(DydxFeatureFlag.abacus_static_typing, default = BuildConfig.DEBUG)
         appConfigsV2.onboardingConfigs.alchemyApiKey = application.getString(R.string.alchemy_api_key)
-        StatsigConfig.useSkip = statsigFlags.isEnabled("ff_skip_migration", default = true)
-        StatsigConfig.ff_enable_evm_swaps = statsigFlags.isEnabled("ff_enable_evm_swaps", default = true)
 
         AsyncAbacusStateManagerV2(
             deploymentUri = deploymentUri,
