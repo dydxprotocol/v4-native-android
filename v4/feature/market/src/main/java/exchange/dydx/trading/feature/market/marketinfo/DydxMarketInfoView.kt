3b81e0e2b6d0ae59547f08528a4a9fadd8c82d49
Rui <rui@dydx.exchange>
2024-12-03 11:25:51 -0800

    Lint

--- v4/feature/market/src/main/java/exchange/dydx/trading/feature/market/marketinfo/DydxMarketInfoView.kt
+++ v4/feature/market/src/main/java/exchange/dydx/trading/feature/market/marketinfo/DydxMarketInfoView.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
