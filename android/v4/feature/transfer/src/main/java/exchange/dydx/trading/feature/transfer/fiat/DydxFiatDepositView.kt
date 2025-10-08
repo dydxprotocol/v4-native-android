dca939f8634ddf495e6f88bcb0eacfb3bc77f749
Rui <rui@dydx.exchange>
2025-10-08 11:26:23 -0700

    Add legal disclaimer

--- android/v4/feature/transfer/src/main/java/exchange/dydx/trading/feature/transfer/fiat/DydxFiatDepositView.kt
+++ android/v4/feature/transfer/src/main/java/exchange/dydx/trading/feature/transfer/fiat/DydxFiatDepositView.kt
@@ -170,6 +170,25 @@ object DydxFiatDepositView : DydxComponent {
             ) {
                 state.ctaAction?.invoke()
             }
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = ThemeShapes.HorizontalPadding)
+                    .padding(bottom = 16.dp),
+                text = state.localizer.localizeWithParams(
+                    path = "APP.DEPOSIT_WITH_FIAT.CONTINUE_TO_DISCLAIMER",
+                    params = mapOf(
+                        "PROVIDER" to (state.providerName ?: "Provider")
+                    )
+                ),
+                textAlign = TextAlign.Center,
+                style = TextStyle.dydxDefault
+                    .themeFont(
+                        fontSize = ThemeFont.FontSize.tiny,
+                    )
+                    .themeColor(foreground = ThemeColor.SemanticColor.text_tertiary),
+            )
         }
     }
 
